85e5c242528b77de6a318830327f004999030c52
davidcoats <david@getsafetycone.com>
2022-01-25 17:25:28 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0037.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0037.X0001
+{
+    /// <summary>
+    /// Basic, Rivet-specific, public, extensions to ICodeFileName and ICodeDirectoryName related extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
